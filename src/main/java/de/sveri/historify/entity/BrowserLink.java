c9f251a913ad8bb346bd8f0f229269ae066423f1
sveri <sveri@sveri.de>
2016-06-18 17:35:34 +0200

    switch generation strategy for postgres

--- src/main/java/de/sveri/historify/entity/BrowserLink.java
+++ src/main/java/de/sveri/historify/entity/BrowserLink.java
@@ -5,6 +5,7 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 import javax.validation.constraints.NotNull;
@@ -15,7 +16,7 @@ import lombok.Data;
 @Entity
 public class BrowserLink {
 	
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Id
     private Long id;
 
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO users (user_name, password, email, role) 
-VALUES ('admin', '$2a$10$1rb2WfWjvLpkxn7/JsnfF.uyA9yBF5XNUn.lzflSR5jjhBHwOdXmy', 'historify@sveri.de', 'ROLE_ADMIN');
+INSERT INTO users (id, user_name, password, email, role) 
+VALUES (1, 'admin', '$2a$10$1rb2WfWjvLpkxn7/JsnfF.uyA9yBF5XNUn.lzflSR5jjhBHwOdXmy', 'historify@sveri.de', 'ROLE_ADMIN');
 
 -- INSERT INTO browser_link (uri, description, title, visited_at, client_id, user_id) VALUES
 -- ('http://tales.sveri.de', 'best page ever', 'sveris tales', 1466227139000, 1, 1)
